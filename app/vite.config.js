0aee3760d0d59192709408ad891b3ec5ed9f6fcb
Seth Feibus <sefeibus@gmail.com>
2022-12-13 19:14:59 -0500

    Move assets, scripts, media

--- app/vite.config.js
+++ app/vite.config.js
@@ -7,9 +7,7 @@ import { urbitPlugin } from '@urbit/vite-plugin-urbit';
 export default ({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd()));
   const SHIP_URL =
-    process.env.SHIP_URL ||
-    process.env.VITE_SHIP_URL ||
-    'http://localhost:8080';
+    process.env.SHIP_URL || process.env.VITE_SHIP_URL || 'http://localhost:80';
   console.log(SHIP_URL);
 
   return defineConfig({
